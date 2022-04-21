944b20fe3f1eeec78cb73936e878acabbe0193ad
rafliyulistiawan <rafliyulistiawan@gmail.com>
2022-04-21 10:22:25 +0700

    input s & r instead of hardcode

--- Tugas_2/2_oop.dart
+++ Tugas_2/2_oop.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'Class/luasLingkaran.dart';
 import 'Class/persegi.dart';
 import 'Class/lingkaran.dart';
 
-void main(List<String> args) async {
+void main(List<String> args) {
   // Class setter getter
   print("Class setter & getter");
   LuasLingkaran luasLingkaran = new LuasLingkaran();
@@ -10,24 +12,32 @@ void main(List<String> args) async {
 
   print("Jari-jari = ${luasLingkaran.getRadius}");
   print(luasLingkaran.luas());
-
   print("");
 
   // Polymorphism & Constructor
   print("Polymorism & Constructor");
 
-  Persegi persegi = new Persegi(5);
-  Lingkaran lingkaran = new Lingkaran(7.0);
+  stdout.write("Sisi persegi: ");
+  String? sPersegi = stdin.readLineSync();
 
-  print("Persegi:");
-  print("Sisi = ${persegi.getN}");
-  print("Keliling = ${persegi.keliling()}");
-  print("Luas = ${persegi.luas()}");
-
+  stdout.write("Jari-jari lingkaran: ");
+  String? rLingkaran = stdin.readLineSync();
   print("");
 
-  print("Lingkaran:");
-  print("Jari-jari = ${lingkaran.getN}");
-  print("Keliling = ${lingkaran.keliling()}");
-  print("Luas = ${lingkaran.luas()}");
+  if (sPersegi != null && rLingkaran != null) {
+    Persegi persegi = new Persegi(double.parse(sPersegi));
+    Lingkaran lingkaran = new Lingkaran(double.parse(rLingkaran));
+
+    print("Persegi:");
+    print("Sisi = ${persegi.getN}");
+    print("Keliling = ${persegi.keliling()}");
+    print("Luas = ${persegi.luas()}");
+
+    print("");
+
+    print("Lingkaran:");
+    print("Jari-jari = ${lingkaran.getN}");
+    print("Keliling = ${lingkaran.keliling()}");
+    print("Luas = ${lingkaran.luas()}");
+  }
 }
